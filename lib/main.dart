6ba3981ac2bbf295c6847380b32fd187fcb7891c
Ainzburn <Ainzburn@gmail.com>
2023-04-27 17:20:40 +0800

    Fetch API and display a few

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'FavPage.dart';
+import 'CatCatalog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const CatCatalogue(),
     );
   }
 }
