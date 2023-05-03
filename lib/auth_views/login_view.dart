42cf7f0ed1452a24097a30b4a890c525b8ce9069
Amarnath911 <drctramar@gmail.com>
2023-05-03 08:47:29 +0800

    Login Page & Register Page Ui Fixed

--- lib/auth_views/login_view.dart
+++ lib/auth_views/login_view.dart
@@ -15,8 +15,6 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-
-  
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -37,98 +35,202 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login'),),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "SIGN IN TO YOUR ACCOUNT",
+          style: TextStyle(
+              fontFamily: 'Raleway',
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w600),
+        ),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(bottomRight: Radius.circular(30))),
+      ),
       body: FutureBuilder(
         future: Firebase.initializeApp(
-                options: DefaultFirebaseOptions.currentPlatform
-              ,),
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: "Enter your email here",
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(40, 50, 40, 0),
+                          child: SizedBox(
+                            child: TextField(
+                              controller: _email,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                hintText: "Enter your email here",
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    width: 3, //<-- SEE HERE
+                                    color: Color.fromRGBO(240, 140, 15, 100),
+                                  ),
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    width: 3, //<-- SEE HERE
+                                    color: Color.fromRGBO(240, 140, 15, 100),
+                                  ),
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                          child: SizedBox(
+                            child: TextField(
+                              controller: _password,
+                              obscureText: true,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              decoration: InputDecoration(
+                                hintText: "Enter password",
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    width: 3, //<-- SEE HERE
+                                    color: Color.fromRGBO(240, 140, 10, 100),
+                                  ),
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    width: 3, //<-- SEE HERE
+                                    color: Color.fromRGBO(240, 140, 15, 100),
+                                  ),
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      hintText: "Enter password",
-                    ),
-                  ),
-                  //continue here
-                  BlocConsumer<LoginStateBloc, LoginState>(
-                    listener: (context, state) {
-                      if (state is AppStateInitial) {}
-                        else if (state is AppStateLoggedIn) {
-                          showDialog<String>(
-                              context: context,
-                              builder: (BuildContext context) => AlertDialog(
-                                title: const Text('Login Successful!'),
-                                content: Text('You are logged in as ${state.email}'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.push(context, 
-                                      MaterialPageRoute(builder: (context) => const MyHomePage()));
-                                    },
-                                    child: const Text('Go to home page'),
+
+                    //continue here
+                    BlocConsumer<LoginStateBloc, LoginState>(
+                        listener: (context, state) {
+                      if (state is AppStateInitial) {
+                      } else if (state is AppStateLoggedIn) {
+                        showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
-                                ],
-                              )
-                            );
-                        }
-                        else if (state is AppStateError) {
-                          showDialog<String>(
-                              context: context,
-                              builder: (BuildContext context) => AlertDialog(
-                                title: Text(state.error.dialogTitle),
-                                content: Text(state.error.dialogText),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.push(context, 
-                                        MaterialPageRoute(builder: (context) => const RegisterView()));
-                                    },
-                                    child: const Text("Sign Up"),
+                                  title: const Text('Login Successful!'),
+                                  content: Text(
+                                      'You are logged in as ${state.email}'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const MyHomePage()));
+                                      },
+                                      child: const Text('Go to home page'),
+                                    ),
+                                  ],
+                                ));
+                      } else if (state is AppStateError) {
+                        showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context, 'Cancel'),
-                                    child: const Text('Cancel'),
-                                  ),
-                                ],
-                              )
-                            );
-                          }
-                      },
-                      builder: (context, state) {
-                        return TextButton(onPressed: () async {
-                          final email = _email.text;
-                          final password = _password.text;
-                          BlocProvider.of<LoginStateBloc>(context).initFirebase(email, password);
-                        }, 
-                        child: const Text("Login"),
+                                  title: Text(state.error.dialogTitle),
+                                  content: Text(state.error.dialogText),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const RegisterView()));
+                                      },
+                                      child: const Text("Sign Up"),
+                                    ),
+                                    TextButton(
+                                      onPressed: () =>
+                                          Navigator.pop(context, 'Cancel'),
+                                      child: const Text('Cancel'),
+                                    ),
+                                  ],
+                                ));
+                      }
+                    }, builder: (context, state) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                            ),
+                          ),
+                          onPressed: () async {
+                            final email = _email.text;
+                            final password = _password.text;
+                            BlocProvider.of<LoginStateBloc>(context)
+                                .initFirebase(email, password);
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.w800,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ),
                       );
-                    }
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context, 
-                      MaterialPageRoute(builder: (context) => const RegisterView()));
-                    },
-                    child: const Text("Not yet registered? Sign up now!"))
-                ],
+                    }),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const RegisterView()));
+                        },
+                        child: const Text("Not yet registered? Sign up now!",
+                            style: TextStyle(
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.w800,
+                                fontSize: 14,
+                               color: Color.fromRGBO(240, 140, 10, 100)))),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      child: SizedBox(
+                        child: Image.asset(
+                          'assets/catimage/c1.png',
+                          height: 200,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               );
-            default: 
+            default:
               return const CircularProgressIndicator();
           }
         },
